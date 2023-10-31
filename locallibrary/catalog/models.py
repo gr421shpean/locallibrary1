c0663cd57ffb2ae68eedbf396e532e62bdd8b07b
gr421shpean <eshpiger33@gmail.com>
2023-10-31 10:53:19 +0700

    Модели, настройка представлений списка, добавление фильтров списков

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -30,6 +30,7 @@ class Genre(models.Model):
         """String for representing the Model object."""
         return self.name
 
+
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
@@ -53,6 +54,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -100,3 +107,4 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
